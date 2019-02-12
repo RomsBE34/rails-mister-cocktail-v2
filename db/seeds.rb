1a271b49a4b57e319d80fd41a5e6988c2ed9e398
Romain Blanco <romain.blanco@gmail.com>
2019-02-12 15:46:29 +0800

    models & db created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+require 'json'
+
+url = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+fetch_url = open(url).read
+parsed_url = JSON.parse(fetch_url)
+
+# binding.pry
+
+results = parsed_url["drinks"]
+
+results.each do |ing|
+ Ingredient.create(name: ing["strIngredient1"])
+end
